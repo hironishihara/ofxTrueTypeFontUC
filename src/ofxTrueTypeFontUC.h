24f884b821151c9307aaba3e94c11f3c3363b172
hiro nishihara <hiro.nishihara@gmail.com>
2013-11-04 16:12:21 +0900

    Revert "code indention"
    This reverts commit 35f5d3d08cccd30a25009629805ef7dce9e4c08d.

--- src/ofxTrueTypeFontUC.h
+++ src/ofxTrueTypeFontUC.h
@@ -13,36 +13,36 @@ const static string OF_TTFUC_MONO = "monospace";
 //--------------------------------------------------
 class ofxTrueTypeFontUC{
 public:
-  ofxTrueTypeFontUC();
-  virtual ~ofxTrueTypeFontUC();
-  
-  // -- default (without dpi), anti aliased, 96 dpi:
-  bool loadFont(const string &filename, int fontsize, bool bAntiAliased=true, bool makeContours=false, float simplifyAmt=0.3, int dpi=0);
+	ofxTrueTypeFontUC();
+	virtual ~ofxTrueTypeFontUC();
+
+	// -- default (without dpi), anti aliased, 96 dpi:
+	bool loadFont(const string &filename, int fontsize, bool bAntiAliased=true, bool makeContours=false, float simplifyAmt=0.3, int dpi=0);
   void reloadFont();
   
   void drawString(const string &utf8_string, float x, float y);
-  void drawStringAsShapes(const string &utf8_string, float x, float y);
+	void drawStringAsShapes(const string &utf8_string, float x, float y);
   
   vector<ofPath> getStringAsPoints(const string &utf8_string, bool vflip=ofIsVFlipped());
-  ofRectangle getStringBoundingBox(const string &utf8_string, float x, float y);
-  
-  bool isLoaded();
-  bool isAntiAliased();
+	ofRectangle getStringBoundingBox(const string &utf8_string, float x, float y);
+	
+	bool isLoaded();
+	bool isAntiAliased();
   
   int getSize();
   float getLineHeight();
   void setLineHeight(float height);
-  float getLetterSpacing();
-  void setLetterSpacing(float spacing);
-  float getSpaceSize();
-  void setSpaceSize(float size);
-  float stringWidth(const string &utf8_string);
-  float stringHeight(const string &utf8_string);
+	float getLetterSpacing();
+	void setLetterSpacing(float spacing);
+	float getSpaceSize();
+	void setSpaceSize(float size);
+	float stringWidth(const string &utf8_string);
+	float stringHeight(const string &utf8_string);
   // get the num of loaded chars
-  int getNumCharacters();
-  ofTextEncoding getEncoding() const;
-  // set the default dpi for all typefaces
-  static void setGlobalDpi(int newDpi);
+	int getNumCharacters();
+	ofTextEncoding getEncoding() const;
+	// set the default dpi for all typefaces
+	static void setGlobalDpi(int newDpi);
   
 private:
   class Impl;
