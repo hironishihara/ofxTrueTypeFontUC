0b5df7332eeac5386f06fd80a073ce00f531bb10
iniwap <iniwaper@gmail.com>
2013-11-29 23:32:35 +0800

    Update ofxTrueTypeFontUC.h

--- src/ofxTrueTypeFontUC.h
+++ src/ofxTrueTypeFontUC.h
@@ -1,48 +1,59 @@
 #pragma once
 
-
 #include <vector>
 #include "ofRectangle.h"
 #include "ofPath.h"
 
-//--------------------------------------------------
-const static string OF_TTFUC_SANS = "sans-serif";
-const static string OF_TTFUC_SERIF = "serif";
-const static string OF_TTFUC_MONO = "monospace";
-
-//--------------------------------------------------
 class ofxTrueTypeFontUC{
+  
 public:
   ofxTrueTypeFontUC();
   virtual ~ofxTrueTypeFontUC();
   
-  // -- default (without dpi), anti aliased, 96 dpi:
-  bool loadFont(const string &filename, int fontsize, bool bAntiAliased=true, bool makeContours=false, float simplifyAmt=0.3, int dpi=0);
+  //set the default dpi for all typefaces.
+  static void setGlobalDpi(int newDpi);
+  
+  // 			-- default (without dpi), anti aliased, 96 dpi:
+  bool loadFont(string filename, int fontsize, bool bAntiAliased=true, bool makeContours=false, float simplifyAmt=0.3, int dpi=0);
   void reloadFont();
+  void unloadFont();
   
-  void drawString(const string &utf8_string, float x, float y);
-  void drawStringAsShapes(const string &utf8_string, float x, float y);
+  void drawString(wstring s, float x, float y);
+  void drawString(string s, float x, float y);
+  void drawStringAsShapes(wstring s, float x, float y);
+  void drawStringAsShapes(string s, float x, float y);
   
-  vector<ofPath> getStringAsPoints(const string &utf8_string, bool vflip=ofIsVFlipped());
-  ofRectangle getStringBoundingBox(const string &utf8_string, float x, float y);
+  ofPath getCharacterAsPoints(wstring character);
+  ofPath getCharacterAsPoints(string character);
+  vector<ofPath> getStringAsPoints(wstring s);
+  vector<ofPath> getStringAsPoints(string s);
   
   bool isLoaded();
   bool isAntiAliased();
   
-  int getSize();
+  int getFontSize();
+  
   float getLineHeight();
   void setLineHeight(float height);
+  
   float getLetterSpacing();
   void setLetterSpacing(float spacing);
+  
   float getSpaceSize();
   void setSpaceSize(float size);
-  float stringWidth(const string &utf8_string);
-  float stringHeight(const string &utf8_string);
-  // get the num of loaded chars
-  int getNumCharacters();
-  ofTextEncoding getEncoding() const;
-  // set the default dpi for all typefaces
-  static void setGlobalDpi(int newDpi);
+  
+  float stringWidth(wstring s);
+  float stringWidth(string s);
+  
+  float stringHeight(wstring s);
+  float stringHeight(string s);
+  
+  ofRectangle getStringBoundingBox(wstring s, float x, float y);
+  ofRectangle getStringBoundingBox(string s, float x, float y);
+  
+  int	getLoadedCharactersCount();
+  int getLimitCharactersNum();
+  void reserveCharacters(int charactersNumber);
   
 private:
   class Impl;
@@ -53,10 +64,11 @@ private:
   void operator=(const ofxTrueTypeFontUC &);
 };
 
-//--------------------------------------------------
+
 namespace util {
   namespace ofxTrueTypeFontUC {
-    basic_string<unsigned int> convUTF8ToUTF32(const string & utf8_string);
-  }
-}
-
+    
+    wstring convToWString(string src);
+    
+  } // ofxTrueTypeFontUC
+} // util
