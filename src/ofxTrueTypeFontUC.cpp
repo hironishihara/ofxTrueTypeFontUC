60e8db2d7bb5130f6c5ed5f5cd58ec41d4765d7c
hiro nishihara <hiro.nishihara@gmail.com>
2014-11-02 23:59:55 +0900

    fix some bugs

--- src/ofxTrueTypeFontUC.cpp
+++ src/ofxTrueTypeFontUC.cpp
@@ -122,10 +122,6 @@ typedef struct {
 } charPropsUC;
 
 
-static bool printVectorInfo = false;
-static int ttfGlobalDpi = 96;
-
-
 //---------------------------------------------------
 class ofxTrueTypeFontUC::Impl {
 public:
@@ -212,143 +208,143 @@ static ofPath makeContoursForCharacter(FT_Face & face);
 
 static ofPath makeContoursForCharacter(FT_Face & face) {
   
-		//int num			= face->glyph->outline.n_points;
-		int nContours	= face->glyph->outline.n_contours;
-		int startPos	= 0;
+  //int num			= face->glyph->outline.n_points;
+  int nContours	= face->glyph->outline.n_contours;
+  int startPos	= 0;
   
-		char * tags		= face->glyph->outline.tags;
-		FT_Vector * vec = face->glyph->outline.points;
+  char * tags		= face->glyph->outline.tags;
+  FT_Vector * vec = face->glyph->outline.points;
   
-		ofPath charOutlines;
-		charOutlines.setUseShapeColor(false);
+  ofPath charOutlines;
+  charOutlines.setUseShapeColor(false);
   
-		for(int k = 0; k < nContours; k++){
-      if( k > 0 ){
-        startPos = face->glyph->outline.contours[k-1]+1;
-      }
-      int endPos = face->glyph->outline.contours[k]+1;
+  for(int k = 0; k < nContours; k++){
+    if( k > 0 ){
+      startPos = face->glyph->outline.contours[k-1]+1;
+    }
+    int endPos = face->glyph->outline.contours[k]+1;
+    
+    if(printVectorInfo_){
+      ofLogNotice("ofxTrueTypeFontUC") << "--NEW CONTOUR";
+    }
+    
+    //vector <ofPoint> testOutline;
+    ofPoint lastPoint;
+    
+    for(int j = startPos; j < endPos; j++){
       
-      if(printVectorInfo){
-        ofLogNotice("ofxTrueTypeFontUC") << "--NEW CONTOUR";
-      }
-      
-      //vector <ofPoint> testOutline;
-      ofPoint lastPoint;
-      
-      for(int j = startPos; j < endPos; j++){
+      if( FT_CURVE_TAG(tags[j]) == FT_CURVE_TAG_ON ){
+        lastPoint.set((float)vec[j].x, (float)-vec[j].y, 0);
+        if(printVectorInfo_){
+          ofLogNotice("ofxTrueTypeFontUC") << "flag[" << j << "] is set to 1 - regular point - " << lastPoint.x <<  lastPoint.y;
+        }
+        if (j==startPos){
+          charOutlines.moveTo(lastPoint/64);
+        }
+        else {
+          charOutlines.lineTo(lastPoint/64);
+        }
         
-        if( FT_CURVE_TAG(tags[j]) == FT_CURVE_TAG_ON ){
-          lastPoint.set((float)vec[j].x, (float)-vec[j].y, 0);
-          if(printVectorInfo){
-            ofLogNotice("ofxTrueTypeFontUC") << "flag[" << j << "] is set to 1 - regular point - " << lastPoint.x <<  lastPoint.y;
+      }else{
+        if(printVectorInfo_){
+          ofLogNotice("ofxTrueTypeFontUC") << "flag[" << j << "] is set to 0 - control point";
+        }
+        
+        if( FT_CURVE_TAG(tags[j]) == FT_CURVE_TAG_CUBIC ){
+          if(printVectorInfo_){
+            ofLogNotice("ofxTrueTypeFontUC") << "- bit 2 is set to 2 - CUBIC";
           }
-          if (j==startPos){
-            charOutlines.moveTo(lastPoint/64);
+          
+          int prevPoint = j-1;
+          if( j == 0){
+            prevPoint = endPos-1;
           }
-          else {
-            charOutlines.lineTo(lastPoint/64);
+          
+          int nextIndex = j+1;
+          if( nextIndex >= endPos){
+            nextIndex = startPos;
+          }
+          
+          ofPoint nextPoint( (float)vec[nextIndex].x,  -(float)vec[nextIndex].y );
+          
+          //we need two control points to draw a cubic bezier
+          bool lastPointCubic =  ( FT_CURVE_TAG(tags[prevPoint]) != FT_CURVE_TAG_ON ) && ( FT_CURVE_TAG(tags[prevPoint]) == FT_CURVE_TAG_CUBIC);
+          
+          if( lastPointCubic ){
+            ofPoint controlPoint1((float)vec[prevPoint].x,	(float)-vec[prevPoint].y);
+            ofPoint controlPoint2((float)vec[j].x, (float)-vec[j].y);
+            ofPoint nextPoint((float) vec[nextIndex].x,	-(float) vec[nextIndex].y);
+            
+            //cubic_bezier(testOutline, lastPoint.x, lastPoint.y, controlPoint1.x, controlPoint1.y, controlPoint2.x, controlPoint2.y, nextPoint.x, nextPoint.y, 8);
+            charOutlines.bezierTo(controlPoint1.x/64, controlPoint1.y/64, controlPoint2.x/64, controlPoint2.y/64, nextPoint.x/64, nextPoint.y/64);
           }
           
         }else{
-          if(printVectorInfo){
-            ofLogNotice("ofxTrueTypeFontUC") << "flag[" << j << "] is set to 0 - control point";
+          
+          ofPoint conicPoint( (float)vec[j].x,  -(float)vec[j].y );
+          
+          if(printVectorInfo_){
+            ofLogNotice("ofxTrueTypeFontUC") << "- bit 2 is set to 0 - conic- ";
+            ofLogNotice("ofxTrueTypeFontUC") << "--- conicPoint point is " << conicPoint.x << conicPoint.y;
           }
           
-          if( FT_CURVE_TAG(tags[j]) == FT_CURVE_TAG_CUBIC ){
-            if(printVectorInfo){
-              ofLogNotice("ofxTrueTypeFontUC") << "- bit 2 is set to 2 - CUBIC";
-            }
+          //If the first point is connic and the last point is connic then we need to create a virutal point which acts as a wrap around
+          if( j == startPos ){
+            bool prevIsConnic = (  FT_CURVE_TAG( tags[endPos-1] ) != FT_CURVE_TAG_ON ) && ( FT_CURVE_TAG( tags[endPos-1]) != FT_CURVE_TAG_CUBIC );
             
-            int prevPoint = j-1;
-            if( j == 0){
-              prevPoint = endPos-1;
-            }
-            
-            int nextIndex = j+1;
-            if( nextIndex >= endPos){
-              nextIndex = startPos;
-            }
-            
-            ofPoint nextPoint( (float)vec[nextIndex].x,  -(float)vec[nextIndex].y );
-            
-            //we need two control points to draw a cubic bezier
-            bool lastPointCubic =  ( FT_CURVE_TAG(tags[prevPoint]) != FT_CURVE_TAG_ON ) && ( FT_CURVE_TAG(tags[prevPoint]) == FT_CURVE_TAG_CUBIC);
-            
-            if( lastPointCubic ){
-              ofPoint controlPoint1((float)vec[prevPoint].x,	(float)-vec[prevPoint].y);
-              ofPoint controlPoint2((float)vec[j].x, (float)-vec[j].y);
-              ofPoint nextPoint((float) vec[nextIndex].x,	-(float) vec[nextIndex].y);
+            if( prevIsConnic ){
+              ofPoint lastConnic((float)vec[endPos - 1].x, (float)-vec[endPos - 1].y);
+              lastPoint = (conicPoint + lastConnic) / 2;
               
-              //cubic_bezier(testOutline, lastPoint.x, lastPoint.y, controlPoint1.x, controlPoint1.y, controlPoint2.x, controlPoint2.y, nextPoint.x, nextPoint.y, 8);
-              charOutlines.bezierTo(controlPoint1.x/64, controlPoint1.y/64, controlPoint2.x/64, controlPoint2.y/64, nextPoint.x/64, nextPoint.y/64);
-            }
-            
-          }else{
-            
-            ofPoint conicPoint( (float)vec[j].x,  -(float)vec[j].y );
-            
-            if(printVectorInfo){
-              ofLogNotice("ofxTrueTypeFontUC") << "- bit 2 is set to 0 - conic- ";
-              ofLogNotice("ofxTrueTypeFontUC") << "--- conicPoint point is " << conicPoint.x << conicPoint.y;
-            }
-            
-            //If the first point is connic and the last point is connic then we need to create a virutal point which acts as a wrap around
-            if( j == startPos ){
-              bool prevIsConnic = (  FT_CURVE_TAG( tags[endPos-1] ) != FT_CURVE_TAG_ON ) && ( FT_CURVE_TAG( tags[endPos-1]) != FT_CURVE_TAG_CUBIC );
-              
-              if( prevIsConnic ){
-                ofPoint lastConnic((float)vec[endPos - 1].x, (float)-vec[endPos - 1].y);
-                lastPoint = (conicPoint + lastConnic) / 2;
-                
-                if(printVectorInfo){
-                  ofLogNotice("ofxTrueTypeFontUC") << "NEED TO MIX WITH LAST";
-                  ofLogNotice("ofxTrueTypeFontUC") << "last is " << lastPoint.x << " " << lastPoint.y;
-                }
-              }
-              else {
-                lastPoint.set((float)vec[endPos-1].x, (float)-vec[endPos-1].y, 0);
+              if(printVectorInfo_){
+                ofLogNotice("ofxTrueTypeFontUC") << "NEED TO MIX WITH LAST";
+                ofLogNotice("ofxTrueTypeFontUC") << "last is " << lastPoint.x << " " << lastPoint.y;
               }
             }
-            
-            //bool doubleConic = false;
-            
-            int nextIndex = j+1;
-            if( nextIndex >= endPos){
-              nextIndex = startPos;
-            }
-            
-            ofPoint nextPoint( (float)vec[nextIndex].x,  -(float)vec[nextIndex].y );
-            
-            if(printVectorInfo){
-              ofLogNotice("ofxTrueTypeFontUC") << "--- last point is " << lastPoint.x << " " <<  lastPoint.y;
-            }
-            
-            bool nextIsConnic = (  FT_CURVE_TAG( tags[nextIndex] ) != FT_CURVE_TAG_ON ) && ( FT_CURVE_TAG( tags[nextIndex]) != FT_CURVE_TAG_CUBIC );
-            
-            //create a 'virtual on point' if we have two connic points
-            if( nextIsConnic ){
-              nextPoint = (conicPoint + nextPoint) / 2;
-              if(printVectorInfo){
-                ofLogNotice("ofxTrueTypeFontUC") << "|_______ double connic!";
-              }
-            }
-            if(printVectorInfo){
-              ofLogNotice("ofxTrueTypeFontUC") << "--- next point is " << nextPoint.x << " " << nextPoint.y;
-            }
-            
-            //quad_bezier(testOutline, lastPoint.x, lastPoint.y, conicPoint.x, conicPoint.y, nextPoint.x, nextPoint.y, 8);
-            charOutlines.quadBezierTo(lastPoint.x/64, lastPoint.y/64, conicPoint.x/64, conicPoint.y/64, nextPoint.x/64, nextPoint.y/64);
-            
-            if( nextIsConnic ){
-              lastPoint = nextPoint;
+            else {
+              lastPoint.set((float)vec[endPos-1].x, (float)-vec[endPos-1].y, 0);
             }
           }
+          
+          //bool doubleConic = false;
+          
+          int nextIndex = j+1;
+          if( nextIndex >= endPos){
+            nextIndex = startPos;
+          }
+          
+          ofPoint nextPoint( (float)vec[nextIndex].x,  -(float)vec[nextIndex].y );
+          
+          if(printVectorInfo_){
+            ofLogNotice("ofxTrueTypeFontUC") << "--- last point is " << lastPoint.x << " " <<  lastPoint.y;
+          }
+          
+          bool nextIsConnic = (  FT_CURVE_TAG( tags[nextIndex] ) != FT_CURVE_TAG_ON ) && ( FT_CURVE_TAG( tags[nextIndex]) != FT_CURVE_TAG_CUBIC );
+          
+          //create a 'virtual on point' if we have two connic points
+          if( nextIsConnic ){
+            nextPoint = (conicPoint + nextPoint) / 2;
+            if(printVectorInfo_){
+              ofLogNotice("ofxTrueTypeFontUC") << "|_______ double connic!";
+            }
+          }
+          if(printVectorInfo_){
+            ofLogNotice("ofxTrueTypeFontUC") << "--- next point is " << nextPoint.x << " " << nextPoint.y;
+          }
+          
+          //quad_bezier(testOutline, lastPoint.x, lastPoint.y, conicPoint.x, conicPoint.y, nextPoint.x, nextPoint.y, 8);
+          charOutlines.quadBezierTo(lastPoint.x/64, lastPoint.y/64, conicPoint.x/64, conicPoint.y/64, nextPoint.x/64, nextPoint.y/64);
+          
+          if( nextIsConnic ){
+            lastPoint = nextPoint;
+          }
         }
-        
-        //end for
       }
-      charOutlines.close();
+      
+      //end for
     }
+    charOutlines.close();
+  }
   
   return charOutlines;
 }
@@ -365,14 +361,14 @@ static set<ofxTrueTypeFontUC*> & all_fonts(){
 void ofUnloadAllFontTextures(){
   set<ofxTrueTypeFontUC*>::iterator it;
   for (it=all_fonts().begin(); it!=all_fonts().end(); ++it) {
-    (*it)->unloadTextures();
+    (*it)->unloadFont();
   }
 }
 
 void ofReloadAllFontTextures(){
   set<ofxTrueTypeFontUC*>::iterator it;
   for (it=all_fonts().begin(); it!=all_fonts().end(); ++it) {
-    (*it)->reloadTextures();
+    (*it)->reloadFont();
   }
 }
 #endif
@@ -784,7 +780,6 @@ vector<ofPath> ofxTrueTypeFontUC::getStringAsPoints(const string &src, bool vfli
       X = 0;
     }
     else if (utf32_src[index] == (unsigned int)' ') {
-      int pID = mImpl->getCharID((unsigned int)'p');
       X += mImpl->cps[charID].setWidth * mImpl->letterSpacing_ * mImpl->spaceSize_;
     }
     else {
@@ -908,8 +903,6 @@ void ofxTrueTypeFontUC::drawString(const string &src, float x, float y){
   GLint index	= 0;
   GLfloat X = x;
   GLfloat Y = y;
-  
-  bool alreadyBinded = mImpl->binded_;
   
   basic_string<unsigned int> utf32_src = convToUTF32(src);
   int len = (int)utf32_src.length();
