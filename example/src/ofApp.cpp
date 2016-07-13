a0f1210d0c3ec6ba03e223e03bd5ddc6562ae823
ChulseungYoo <csyoo@imlabworld.com>
2016-07-13 10:55:08 +0900

    example vs project added

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -1,10 +1,10 @@
-#include "testApp.h"
+﻿#include "ofApp.h"
 
 //--------------------------------------------------------------
-void testApp::setup(){
+void ofApp::setup(){
   myFont.loadFont("dummyFont.ttf", 64, true, true);
   
-  sampleString = "あいうえお";
+  sampleString.append("あいうえお", 10);
   
   p1 = ofPoint(100, 200);
   p2 = ofPoint(200, 300);
@@ -18,12 +18,12 @@ void testApp::setup(){
 }
 
 //--------------------------------------------------------------
-void testApp::update(){
+void ofApp::update(){
   
 }
 
 //--------------------------------------------------------------
-void testApp::draw(){
+void ofApp::draw(){
   string fpsStr = "frame rate: " + ofToString(ofGetFrameRate(), 2);
   ofDrawBitmapString(fpsStr, 100, 100);
   
@@ -49,46 +49,46 @@ void testApp::draw(){
 }
 
 //--------------------------------------------------------------
-void testApp::keyPressed(int key){
+void ofApp::keyPressed(int key){
   
 }
 
 //--------------------------------------------------------------
-void testApp::keyReleased(int key){
+void ofApp::keyReleased(int key){
   
 }
 
 //--------------------------------------------------------------
-void testApp::mouseMoved(int x, int y ){
+void ofApp::mouseMoved(int x, int y ){
   
 }
 
 //--------------------------------------------------------------
-void testApp::mouseDragged(int x, int y, int button){
+void ofApp::mouseDragged(int x, int y, int button){
   
 }
 
 //--------------------------------------------------------------
-void testApp::mousePressed(int x, int y, int button){
+void ofApp::mousePressed(int x, int y, int button){
   
 }
 
 //--------------------------------------------------------------
-void testApp::mouseReleased(int x, int y, int button){
+void ofApp::mouseReleased(int x, int y, int button){
   
 }
 
 //--------------------------------------------------------------
-void testApp::windowResized(int w, int h){
+void ofApp::windowResized(int w, int h){
   
 }
 
 //--------------------------------------------------------------
-void testApp::gotMessage(ofMessage msg){
+void ofApp::gotMessage(ofMessage msg){
   
 }
 
 //--------------------------------------------------------------
-void testApp::dragEvent(ofDragInfo dragInfo){
+void ofApp::dragEvent(ofDragInfo dragInfo){
   
 }
